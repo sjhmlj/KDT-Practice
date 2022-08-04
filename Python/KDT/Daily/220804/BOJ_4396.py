8379de1936b178c2c596cf645d8f8b961de5ebe8
techtaek54 <techtaek54@gmail.com>
2022-08-04 22:51:53 +0900

    add BOJ_4396.py 풀이2

--- Python/KDT/Daily/220804/BOJ_4396.py
+++ Python/KDT/Daily/220804/BOJ_4396.py
@@ -1,47 +1,42 @@
-'''
-Q. 지뢰 찾기
-- 상하좌우, 대각선으로 인접한 8개의 칸에 대한 알림
-- 즉 시계방향(또는 반시계)로 탐색을 진행
-'''
 import sys
-
 
 input = sys.stdin.readline
 
-n = int(input())
-# . : 지뢰 없음 / * : 지뢰
-grid = [list(input().rstrip()) for _ in range(n)]
-# x : 열림 / . : 열리지 않음
-game = [list(input().rstrip()) for _ in range(n)]
-
+# input
+N = int(input())
+mine = [list(input().rstrip()) for _ in range(N)]
+play = [list(input().rstrip()) for _ in range(N)]
+ans = [['.' for _ in range(N)] for _ in range(N)]
+# 방향 벡터 
 dx = [-1, 0, 1, 1, 1, 0, -1, -1]
 dy = [1, 1, 1, 0, -1, -1, -1, 0]
 
-ans = [['.' for _ in range(n)] for _ in range(n)]
 flag = False
 
-for i in range(n):
-    for j in range(n):
-        if game[i][j] == 'x':
+for i in range(N):
+    cnt = 0
+    for j in range(N):
+        if play[i][j] == 'x':
             cnt = 0
-
-            if grid[i][j] == '*':
+            
+            if mine[i][j] == '*':
                 flag = True
             
             for k in range(8):
                 x = i + dx[k]
                 y = j + dy[k]
 
-                if 0 <= x < n and 0 <= y < n:
-                    if grid[x][y] == '*':
+                if 0 <= x < N and 0 <= y < N:
+                    if mine[x][y] == '*':
                         cnt += 1
             ans[i][j] = str(cnt)
-
 if flag:
-    for i in range(n):
-        for j in range(n):
-            if grid[i][j] == '*':
+    for i in range(N):
+        for j in range(N):
+            if mine[i][j] == '*':
                 ans[i][j] = '*'
 
-for i in range(n):
+for i in range(N):
     print(''.join(ans[i]))
+
+        
